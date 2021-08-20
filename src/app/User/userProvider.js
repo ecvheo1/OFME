fa431d3353bbdd8ec445eca5a2ff4eb94047daff
ChaeMin0215 <=>
2021-08-20 20:38:23 +0900

    kakao social login API 추가

--- src/app/User/userProvider.js
+++ src/app/User/userProvider.js
@@ -67,3 +67,11 @@ exports.getUserNickname = async function(userId) {
 
   return response(baseResponse.SUCCESS, getUserNickname);
 }
+
+exports.selectUsetId = async function(socialId) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const userIdResult = await userDao.selectUsetId(connection, socialId);
+  connection.release();
+
+  return userIdResult;
+}
