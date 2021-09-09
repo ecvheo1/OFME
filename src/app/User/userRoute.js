3973e22dfe85f1917d8f78e387b582d69ea85910
ecvheo1 <ecvheo@naver.com>
2021-09-09 16:32:01 +0900

    fix apple login api

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -3,6 +3,13 @@ module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
     const passport = require('passport');
 
+    const bodyParser = require('body-parser');
+    const fs = require("fs");
+    const AppleAuth = require("apple-auth");
+    const config = fs.readFileSync("config/appleConfig.json");
+    const auth = new AppleAuth(config, "config/AuthKey.p8");
+    const jwt = require("jsonwebtoken");
+
     // 회원가입 API
     app.post('/sign-up', user.postUsers);
 
@@ -36,5 +43,42 @@ module.exports = function(app){
 
     // 닉네임 중복확인 API
     app.get('/login/nickname/:nicknames', user.getNickname);
+
+
+
+
+
+    
+    // 애플 로그인 Web Test
+    app.get('/test/apple/login', (req,res) => {
+        console.log(Date().toString() + "GET /test/apple/login");
+        res.send(`<a href="${auth.loginURL()}">Sign in with Apple</a>`);
+    });
+
+    app.get('/token', (req,res) => {
+        res.send(auth._tokenGenerator.generate());
+    });
+
+    app.post('/auth', bodyParser(), async (req, res) => {
+        try {
+            console.log( Date().toString() + "POST /auth");
+            const response = await auth.accessToken(req.body.code);
+            const idToken = jwt.decode(response.id_token);
+    
+            const user = {};
+            user.id = idToken.sub;
+    
+            if (idToken.email) user.email = idToken.email;
+            if (req.body.user) {
+                const { name } = JSON.parse(req.body.user);
+                user.name = name;
+            }
+    
+            res.json(user);
+        } catch (ex) {
+            console.error(ex);
+            res.send("An error occurred!");
+        }
+    });
 
 };
